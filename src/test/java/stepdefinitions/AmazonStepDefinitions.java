52594e096383678c1da87a6cdc33862a14d6721c
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-19 22:02:07 +0300

    ilk olusturma

---
+++ src/test/java/stepdefinitions/AmazonStepDefinitions.java
@@ -0,0 +1,42 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AmazonStepDefinitions {
+
+    @Given("kullanici amazon sayfasina gider")
+    public void kullanici_amazon_sayfasina_gider() {
+
+    }
+
+    @Given("iPhone icin arama yapar")
+    public void i_phone_icin_arama_yapar() {
+
+    }
+
+    @Then("sonuclarin Iphone icerdigini test eder")
+    public void sonuclarin_iphone_icerdigini_test_eder() {
+
+    }
+
+    @Given("tea pot icin arama yapar")
+    public void tea_pot_icin_arama_yapar() {
+
+    }
+
+    @Then("sonuclarin tea pot icerdigini test eder")
+    public void sonuclarin_tea_pot_icerdigini_test_eder() {
+
+    }
+
+    @Given("flower icin arama yapar")
+    public void flower_icin_arama_yapar() {
+
+    }
+
+    @Then("sonuclarin flower icerdigini test eder")
+    public void sonuclarin_flower_icerdigini_test_eder() {
+
+    }
+}
